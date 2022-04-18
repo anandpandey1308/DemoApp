82a17739e7f164fdd477af69e45b4347359c6750
anandpandey1308 <53857568+anandpandey1308@users.noreply.github.com>
2022-04-18 22:16:05 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import LoginPage from "./components/pages/LoginPage";
+import HomePage from "./components/pages/HomePage";
+
+import "./App.css";
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={LoginPage} />
+          <Router path="/home" component={HomePage} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
